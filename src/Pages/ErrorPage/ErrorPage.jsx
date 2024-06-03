a85f61be56a707bbe8b4fb987b02a0708d7007c2
Idba1 <moniraislamself@gmail.com>
2024-06-03 22:53:11 +0600

    error page

---
+++ src/Pages/ErrorPage/ErrorPage.jsx
@@ -0,0 +1,33 @@
+
+const ErrorPage = () => {
+    return (
+      <section className='bg-white '>
+        <div className='container min-h-screen px-6 py-12 mx-auto lg:flex lg:items-center lg:gap-5'>
+          <div className='wf-ull lg:w-1/2'>
+            <p className='text-4xl font-medium text-gray- '>404 error</p>
+            <h1 className='mt-3 text-2xl font-semibold text-gray-800 dark:text-white md:text-3xl'>
+              Page not found
+            </h1>
+            <p className=' text-gray-500 dark:text-gray-400'>
+              Sorry, the page you are looking for doesnt exist.Here are some
+              helpful links:
+            </p>
+  
+            <div className='flex items-center mt-6 gap-x-3'>
+             
+            </div>
+          </div>
+  
+          <div className='relative w-full mt-8 lg:w-1/2 lg:mt-0'>
+            <img
+              className=' w-full lg:h-[32rem] h-80 md:h-96 rounded-lg object-cover '
+              src='https://i.ibb.co/ZG9KHnZ/error-404-6052476-640.png'
+              alt=''
+            />
+          </div>
+        </div>
+      </section>
+    )
+  }
+  
+  export default ErrorPage
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,16 +1,22 @@
 import { createBrowserRouter } from 'react-router-dom'
 import Main from '../Layout/Main';
+import ErrorPage from '../Pages/ErrorPage/ErrorPage.jsx';
+import Home from '../Components/Home/Home'
 
 const Routes = createBrowserRouter([
-    {
-      path: '/',
-      element: <Main></Main>,
-      errorElement:<h1>error page</h1> ,
-      children: [
+  {
+    path: '/',
+    element: <Main></Main>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>,
+      },
 
-      ],
-    },
-  ])
+    ],
+  },
+])
 
 
 export default Routes;
