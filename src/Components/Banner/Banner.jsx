fde94f1bf6a4bb7d2d8c14185d72dc225559343e
Idba1 <moniraislamself@gmail.com>
2024-06-04 11:55:06 +0600

    banner section

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -1,24 +1,24 @@
-import './banner.css'
 const Banner = () => {
- 
+
 
   return (
-    <section className="mt-10 bg-cover bg-center h-96 flex items-center justify-center banner">
-      <div className="overlay">
-        <div className="items-center text-white">
-          <h1 className="title">Welcome to IDEA BATTLE</h1>
-          <p className="subtitle">Find and participate in exciting contests</p>
-          <div className="search-bar">
-            <input
-              type="text"
-              value=""
-              placeholder="Search contests by tags..."
-            />
-            <button>Search</button>
+    <>
+      <div className="hero min-h-96 mt-10" style={{ backgroundImage: 'url(https://i.ibb.co/gvBdPPZ/pexels-photo-1309408.jpg)' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <div className="flex justify-center items-center">
+              <label className="input input-bordered flex items-center gap-2">
+                <input type="text" className="grow" placeholder="Search contests by tags..." />
+                <button className="btn btn-primary">Search</button>
+              </label>
+            </div>
           </div>
         </div>
       </div>
-    </section>
+    </>
   );
 };
 
